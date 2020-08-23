57a57867f373058ea48c65a52395909211dfcd64
fhzno <70105195+fhzno@users.noreply.github.com>
2020-08-23 14:24:56 -0400

    Functional component

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,40 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Header, Footer, Main } from '../components';
 import { apiUrl } from '../utils/constants';
 
-interface IState {
-  current_price: string;
-}
+const Landing: React.FC = () => {
+  const [currentPrice, setCurrentPrice] = useState('');
 
-class Landing extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      current_price: '',
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(apiUrl);
+      setCurrentPrice(result.data.tickers[0].converted_last['usd']);
     };
-  }
+    fetchData();
+  }, []);
 
-  componentDidMount() {
-    this.getCurrentPrice();
-  }
-
-  async getCurrentPrice() {
-    const response = await fetch(apiUrl);
-    const json = await response.json();
-    this.setState({ current_price: json.tickers[0].converted_last['usd'] });
-  }
-
-  render() {
-    const { current_price } = this.state;
-    return (
-      <>
-        <body className='flex flex-col min-h-screen'>
-          <Header price={current_price} />
-          <Main />
-          <Footer />
-        </body>
-      </>
-    );
-  }
-}
+  return (
+      <div className='flex flex-col min-h-screen'>
+        <Header price={currentPrice} />
+        <Main />
+        <Footer />
+      </div>
+  );
+};
 export default Landing;
